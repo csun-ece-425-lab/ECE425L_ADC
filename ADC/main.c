50364cea25feffe8d4ce74b2bd768724539f29d4
ananas-csun <ananas.ece25@gmail.com>
2024-11-24 15:35:19 -0800

    Added template for the ADC lab

---
+++ ADC/main.c
@@ -0,0 +1,22 @@
+/*
+ * @file main.c
+ *
+ * @brief Main source code for the ADC program.
+ *
+ * This file contains the main entry point and function definitions for the ADC program.
+ *
+ * It interfaces with the following:
+ *  - EduBase Board Potentiometer (PE2)
+ *  - EduBase Board Analog Light Sensor (PE1)
+ *
+ * @author
+ */
+
+int main(void)
+{
+	
+	while(1)
+	{		
+
+	}
+}
