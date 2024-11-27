8bb43606087679394ced9f6b277f2ab40c8a2f3d
ananas-csun <ananas.ece25@gmail.com>
2024-11-27 08:30:17 -0800

    Updated code documentation

--- ADC/ADC.h
+++ ADC/ADC.h
@@ -1,11 +1,12 @@
 /**
  * @file ADC.h
+ *
  * @brief Header file for the ADC driver.
  *
  * This file contains the function definitions for the ADC driver.
  *
  * ADC Module 0 is used to sample the potentiometer and the analog
- * light sensor that are connected on the EduBase board. The ADC module
+ * light sensor that are connected on the EduBase board. ADC Module 0
  * is configured to sample the potentiometer first and the light sensor after.
  *
  * After the light sensor is sampled, an interrupt signal is set to 
